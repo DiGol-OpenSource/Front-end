54864391aabb0dc40ba1c542a1ba40547208c55e
Nilton Torres <u201924058@upc.edu.pe>
2022-05-20 19:19:46 -0500

    feat: Added service for sports fields

--- src/app/customer-home/service/sports-fields.service.ts
+++ src/app/customer-home/service/sports-fields.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpErrorResponse, HttpHeaders} from "@angular/common/http";
+import {catchError, Observable, retry, throwError} from "rxjs";
+import {Customer} from "../../customer/model/customer";
+import {SportField} from "../model/sport-field";
 
 @Injectable({
   providedIn: 'root'
 })
 export class SportsFieldsService {
+  basePath='http://localhost:3000/customer-home';
+  httpOptions={
+    headers:new HttpHeaders({
+      'Content-Type':'application/json',
+    })
+  }
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  //API Error Handling
+  handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      console.log(`An error ocurred: ${error.error.message}`);
+    } else {
+      console.error(
+          `Backend return code ${error.status}, body was: ${error.error}`
+      );
+    }
+    return throwError( () => new Error(`Something happened with request, please try again later`));
+  }
+  getAll(): Observable<SportField> {
+    return this.http.get<SportField>(this.basePath, this.httpOptions)
+        .pipe(
+            retry(2),
+            catchError(this.handleError)
+        );
+  }
+  create(item: any): Observable<SportField> {
+    return this.http.post<SportField>(this.basePath, JSON.stringify(item), this.httpOptions)
+        .pipe(
+            retry(2),
+            catchError(this.handleError));
+  }
+
 }
