a933f034b26408dc9a78a4ddaa1406e6bb525038
Nilton Torres <u201924058@upc.edu.pe>
2022-05-26 23:11:16 -0500

    feat: Added booking steps

--- src/app/customer/pages/customer-reservation/customer-reservation.component.ts
+++ src/app/customer/pages/customer-reservation/customer-reservation.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import {SportField} from "../../model/sport-field";
 import {CustomerHomeComponent} from "../customer-home/customer-home.component";
 import {SportsFieldsService} from "../../service/sports-fields.service";
@@ -6,7 +6,6 @@ import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {Reservation} from "../../../reservations/model/reservation";
 import {SendReservationService} from "../../service/send-reservation.service";
 import {MatTableDataSource} from "@angular/material/table";
-
 
 @Component({
   selector: 'app-customer-reservation',
@@ -30,6 +29,8 @@ export class CustomerReservationComponent implements OnInit {
   reservationData!:Reservation;
   dataSource!: MatTableDataSource<any>;
 
+
+
   ngOnInit(): void {
     this.firstFormGroup = this._formBuilder.group({
       firstCtrl: ['', Validators.required],
@@ -52,6 +53,11 @@ export class CustomerReservationComponent implements OnInit {
   displayReservation(){
     this.display = this.data.length <= 0;
   }
-
-
+  addReservation(){
+    this.reservationData.id = 0;
+    this.sendReservationService.create(this.reservationData).subscribe( (response: any) => {
+      this.dataSource.data.push({...response});
+      this.dataSource.data = this.dataSource.data.map((o: any)=> {return o;})
+    });
+  }
 }
--- src/app/reservations/model/reservation.ts
+++ src/app/reservations/model/reservation.ts
@@ -2,6 +2,7 @@ export interface Reservation{
   id:number;
   customer:string;
   date:string;
+  hour:string,
   prepayment:number;
 
 }
